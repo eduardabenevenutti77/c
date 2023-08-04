1118117c30324ec58dcd666a35a0c7ac00cdd092
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-08-03 21:13:27 -0300

    Update funcoes_ponteiros.c

--- 0308/funcoes_ponteiros.c
+++ 0308/funcoes_ponteiros.c
@@ -6,21 +6,21 @@ ponteiros:
 -> precisamos declarar ele antes de utilizar;
 -> usa-se * para informar ao sistema que ali para reservar o endereço de outra váriavel;
 -> tem que tomar cuidado quando for utilizar!!
--> endereço da váriavel é dado pelo seguinte simbolo = & (&i) - considere-se i como o nome da váriavel;
+-> endereço da variável é dado pelo seguinte simbolo = & (&i) - considere-se i como o nome da variável;
 
 	mostrar o endereço da váriavel:
-	
+
 int idade = 44;
 int *p = &idade; - *p = 2 (posição na mémoria ram)
 
-	váriaveis normais:
-	
+	variáveis normais:
+
 int num;
 float nota;
 struct pessoa p;
 
 	declaração de ponteiros:
-	
+
 int *num;
 float *nota;
 struct pessoa *p;
@@ -28,17 +28,51 @@ struct pessoa *p;
 funções: ;
 mémoria ram: é uma sequência de bytes;
 */
+
 #include <stdio.h>
 
-void main(){
-	printf("\n\t aula sobre a utilização de ponteiros e funções!")
+void main() {
+	system("chcp 65001");
+	printf("\n\t \e[33;1maula sobre a utilização de ponteiros e funções!\e[33;0m\n");
+
+	/*int i = 10; //variável I inicia com o valor de 10
+	int *p; //ponteiro para int
+
+	p = &i; //ponteiro p aponta para o valor de i
+
+	printf("\n O valor de i é: \e[33;1m%d\e[33;0m", i);
+	printf("\n O valor apontado por p é: \e[33;1m%d\e[33;0m", *p);
+	printf("\n O endereço de i armazenado em p é: \e[33;1m%d\e[33;0m\n", p);
+
+	*p = 20; //modificando o valor do ponteiro
+
+	printf("\n\t \e[33;1mMODIFICANDO O VALOR DO PONTEIRO\e[33;0m\n");
+	printf("\n O valor de i é: \e[33;1m%d\e[33;0m", i);
+	printf("\n O valor apontado por p é: \e[33;1m%d\e[33;0m", *p);
+	printf("\n O endereço de i armazenado em p é: \e[33;1m%d\e[33;0m", p);
 	
-	int i = 10;
+	int i = 100;
 	int *p;
 	
 	p = &i;
+	printf("\n O valor de i é: \e[33;1m%d\e[33;0m", i);
+	printf("\n O valor apontado por p é: \e[33;1m%d\e[33;0m\n", *p);
 	
-	printf("\n O valor de i é: %d", i);
-	printf("\n O valor apontado por p é: ", *p);
-	printf("\n O endereço de i armazenado em p é: ", );
+	*p = 12;
+	printf("\n O valor de i é: \e[33;1m%d\e[33;0m", i);
+	printf("\n O valor apontado por p é: \e[33;1m%d\e[33;0m\n", *p);
+	
+	i = 547;
+	printf("\n O valor de i é: \e[33;1m%d\e[33;0m", i);
+	printf("\n O valor apontado por p é: \e[33;1m%d\e[33;0m\n", *p);*/
+	
+	int i;
+	int *p;
+	printf("\n \e[33;1m%d %d\e[33;0m",p,&i);
+	
+	p = &i;
+	printf("\n \e[33;1m%d %d\e[33;0m",p,&i);
+	printf("\n\n");
+	
+	system("pause");
 }
