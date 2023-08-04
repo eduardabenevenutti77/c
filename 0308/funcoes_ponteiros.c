e080e343574727575f9af4019ed603ca1bb4c7de
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-08-03 21:42:31 -0300

    Update funcoes_ponteiros.c

--- 0308/funcoes_ponteiros.c
+++ 0308/funcoes_ponteiros.c
@@ -25,7 +25,6 @@ int *num;
 float *nota;
 struct pessoa *p;
 
-funções: ;
 mémoria ram: é uma sequência de bytes;
 */
 
@@ -64,7 +63,7 @@ void main() {
 	
 	i = 547;
 	printf("\n O valor de i é: \e[33;1m%d\e[33;0m", i);
-	printf("\n O valor apontado por p é: \e[33;1m%d\e[33;0m\n", *p);*/
+	printf("\n O valor apontado por p é: \e[33;1m%d\e[33;0m\n", *p);
 	
 	int i;
 	int *p;
@@ -72,6 +71,47 @@ void main() {
 	
 	p = &i;
 	printf("\n \e[33;1m%d %d\e[33;0m",p,&i);
+	printf("\n\n");
+	
+	int valor = 27;
+	int *ptr;
+	
+	ptr = &valor;
+	
+	printf("\n O conteúdo da variável valor: \e[33;1m%d\e[33;0m", valor);
+	printf("\n Endereço da variável valor: \e[33;1m%x\e[33;0m", &valor);
+	printf("\n Conteúdo da variável ponteiro: \e[33;1m%x\e[33;0m", ptr);
+	
+	int *p, *p1;
+	int i = 10;
+	p = &i;
+	p1 = p;
+	
+	printf("\n=> \e[33;1m%d\e[33;0m", *p1);
+	
+	int j = 20;
+	p1 = &j;
+	printf("\n=> \e[33;1m%d\e[33;0m", *p1);
+	*p1 = *p;
+	printf("\n=> \e[33;1m%d\e[33;0m", *p1);*/
+	
+	//adição e subtração de ponteiros
+	
+	int *p;
+	int i=10, j=20, k=30;
+	p=&j;
+	printf("\n 1º O valor de *p = \e[33;1m%d\e[33;0m",*p);
+	printf("\n 1º Endereço de p = \e[33;1m%d\e[33;0m\n", p);
+	
+	p++;
+	printf("\n 2º O valor de *p = \e[33;1m%d\e[33;0m", *p);
+	printf("\n 2º Endereço de p = \e[33;1m%d\e[33;0m\n", p);
+	
+	printf("\n 3º Endereço de p = \e[33;1m%d\e[33;0m\n", --p);
+	
+	printf("\n 4º Endereço de i = \e[33;1m%d\e[33;0m", i);
+	printf("\n 4º Endereço de k = \e[33;1m%d\e[33;0m", k);
+	printf("\n 4º Endereço de j = \e[33;1m%d\e[33;0m", j);
 	printf("\n\n");
 	
 	system("pause");
